ceb9d7376c0b48e2766b68c32aa7dde73be81439
Matheus Santos <matheussantosspbr@gmail.com>
2023-01-06 12:38:32 -0300

    resolução de erro

--- api/index.py
+++ api/index.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI
 # from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from conexao import Conexao
+from conexao import Conexao as Conexao
 
 db = Conexao()
 
