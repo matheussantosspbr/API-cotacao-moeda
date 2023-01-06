71f6f587bc69c925393d38df94ad3a4b9cfd7ad2
Matheus Santos <matheussantosspbr@gmail.com>
2023-01-06 13:58:17 -0300

    resolução de erro

--- api/conexao.py
+++ api/conexao.py
@@ -9,11 +9,13 @@ class BancoDeDados:
         con = self.con
         db = con.cursor()
         db.execute("SELECT * FROM precos ORDER BY created_date DESC LIMIT 1")
-        return db.fetchall()
+        res = db.fetchall()
+        db.close()
+        return res
 
     def EnviarUltimoValor(self,precos):
         con = self.con
-        db=con.cursor()
+        db = con.cursor()
         dolarParaReal = precos.dolarParaReal * 10000
         realParaDolar = precos.realParaDolar * 10000
         euroParaReal = precos.euroParaReal * 10000 
@@ -22,20 +24,17 @@ class BancoDeDados:
         dolarParaEuro = precos.dolarParaEuro * 10000
 
         query = """INSERT INTO precos (USD_BRL, BRL_USD, EUR_BRL, BRL_EUR, EUR_USD, USD_EUR) VALUES('%i','%i','%i','%i','%i','%i' )""" % (dolarParaReal,realParaDolar, euroParaReal, realParaEuro, euroParaDolar, dolarParaEuro)
-        print( query)
-            
         try:
             db.execute(query)
-            con.commit()
+            db.commit()
             db.close()
             return{
                 'status': 201,
                 'message':'Valor criado com sucesso'
             }
         except:
-            con.rollback()
+            db.rollback()
             db.close()
-            
             return {
                 'status': 400,
                 'message': "Erro ao tentar enviar um valor"
