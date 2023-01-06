8fae520168db0f0061074f26f7c9a91e64c46dd8
Matheus Santos <matheussantosspbr@gmail.com>
2023-01-06 11:19:00 -0300

    first commit

--- api/index.py
+++ api/index.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI
 # from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-# from conexao import PegarUltimoValor
+from conexao import PegarUltimoValor
 
 # class PropsPrecos(BaseModel):
 #     dolarParaReal: float
@@ -32,14 +32,12 @@ app.add_middleware(
 
 @app.get("/")
 async def home():
-    return {"status": True}
-
-    # precos = PegarUltimoValor()
-    # return {
-    #     "dolarParaReal":precos[0][1] / 10000,
-    #     "realParaDolar":precos[0][2] / 10000,
-    #     "euroParaReal":precos[0][3]  / 10000,
-    #     "realParaEuro":precos[0][4]  / 10000,
-    #     "euroParaDolar":precos[0][5] / 10000,
-    #     "dolarParaEuro":precos[0][6] / 10000
-    # }
+    precos = PegarUltimoValor()
+    return {
+        "dolarParaReal":precos[0][1] / 10000,
+        "realParaDolar":precos[0][2] / 10000,
+        "euroParaReal":precos[0][3]  / 10000,
+        "realParaEuro":precos[0][4]  / 10000,
+        "euroParaDolar":precos[0][5] / 10000,
+        "dolarParaEuro":precos[0][6] / 10000
+    }
