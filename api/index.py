9deb32e9acaa01101d56b8ecae10479e9bd556a1
Matheus Santos <matheussantosspbr@gmail.com>
2023-01-24 12:55:02 -0300

    adicionando uma nova origem

--- api/index.py
+++ api/index.py
@@ -22,7 +22,7 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*'],
     allow_credentials=True,
-    allow_origins=['https://precohoje.info']
+    allow_origins=['https://precohoje.info', 'http://localhost:5173']
 
 )
     
